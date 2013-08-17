b8858ffd99b5f09882a0a909090dfea5f19a45eb
abdulmatinshah <abdulmateenshah@gmail.com>
2013-08-17 10:52:14 -0700

    add devise

--- Gemfile
+++ Gemfile
@@ -34,8 +34,8 @@ end
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
-gem "devise"
-
+gem 'devise', '~>3.0.0'
+gem "simple_form"
 # Use unicorn as the app server
 # gem 'unicorn'
 
---
+++ lib/user_sanitizer.rb
@@ -0,0 +1,10 @@
+class User::ParameterSanitizer < Devise::ParameterSanitizer
+
+protected
+  def sign_up
+    default_params.permit(:first_name, :last_name, :profile_name, :email, :password, :password_confirmation, :current_password)
+  end
+  def account_update
+    default_params.permit(:first_name, :last_name, :profile_name, :email, :password, :password_confirmation, :current_password)
+  end
+end
